f63d599a3a0f9b91977ebd4017f16d7529035db1
uach2016 <uacaminoherrera@gmail.com>
2017-01-20 14:19:35 +0000

    first commit

---
+++ weather.js
@@ -0,0 +1,14 @@
+// $(document).ready(function() {
+//   $("button").click(function() {
+//     $.ajax({
+//       type: "GET",
+//       dataType: "jsonp",
+//       url: "http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=fdd8080861e53a80500472420b920290",
+//       crossDomain: true,
+//       success: function(result) {
+//                 console.log(result);
+
+//                 }
+//     });
+//   });
+// });
